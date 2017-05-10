3ffd701dc65e09e97f89fe017c5a296d811eff36
Benjamin Swineford <benjamin@swineford.me>
2017-05-09 21:49:17 -0500

    fixed server to hand off routing to react router

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.set('port', (process.env.PORT || 3001));
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+    });
 }
 
 app.listen(app.get('port'), () => {
