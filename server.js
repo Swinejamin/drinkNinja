e04f02673c5cf1c6852f1a49ece1e1a64f81a805
Benjamin Swineford <benjamin@swineford.me>
2017-05-09 22:52:07 -0500

    honing in on build path

--- server.js
+++ server.js
@@ -11,16 +11,18 @@ app.set('port', (process.env.PORT || 3001));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('/*', (req, res) => {
-        fs.readdir(__dirname, (err, files) => {
+        const target = __dirname + '/client/build';
+        fs.readdir(target, (err, files) => {
             files.forEach(file => {
                 console.log(file);
             });
         });
         console.log(__dirname);
-        res.sendFile(__dirname + '/index.html');
+        res.sendFile(target + '/index.html');
     });
 }
 
 app.listen(app.get('port'), () => {
+
     console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
